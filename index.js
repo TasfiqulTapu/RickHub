dcde18cb127b7b57ea579f8d10cac326ecadff4e
Tapu <66732331+TasfiqulTapu@users.noreply.github.com>
2022-07-04 23:57:08 +0600

    🕺 Inital version

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("Rick is here")
+const title = document.getElementsByTagName('title')[0];
+const page = document.getElementsByTagName('body')[0];
+const INDEX_URL = browser.runtime.getURL("index.html");
+
+title.innerText = "Free Rick Videos & Rick Movies - Rick, 🔥, Rick Tube | Rickhub"
+page.innerHTML = `<iframe src="${INDEX_URL}" style="min-width: 100vw;min-height: 100vh;" frameborder="0"></iframe>`;
